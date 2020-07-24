216c9456907afbc98ed7bcd208a02adc4976ee06
SherifGhoz <ghozdev@gmail.com>
2020-07-24 23:21:02 +0200

    initial elkalam

---
+++ components/Elkalam/Dorar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div v-html="result"></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      result: null
+    }
+  },
+  mounted() {
+    this.getData()
+  },
+  methods: {
+    getData() {
+      this.$jsonp('https://dorar.net/dorar_api.json', {
+        skey: 'بني الإسلام'
+      })
+        .then((json) => {
+          this.result = json.ahadith.result
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    }
+  }
+}
+</script>
+
+<style></style>
--- pages/elkalam.vue
+++ pages/elkalam.vue
@@ -1,7 +1,36 @@
 <template>
-  <h2>الكلام دا صح؟</h2>
+  <v-container>
+    <v-tabs v-model="tab" :height="100" background-color="primary" grow="" dark>
+      <v-tab>الدرر السنية</v-tab>
+      <v-tab>الكلام دا صح؟</v-tab>
+    </v-tabs>
+    <v-tabs-items v-model="tab">
+      <v-tab-item><Dorar /></v-tab-item>
+      <v-tab-item>الكلام دا صح ؟</v-tab-item>
+    </v-tabs-items>
+  </v-container>
 </template>
 
 <script>
-export default {}
+import Dorar from '@/components/Elkalam/Dorar'
+
+export default {
+  components: {
+    Dorar
+  },
+  data() {
+    return {
+      tab: null
+    }
+  }
+}
 </script>
+
+<style scoped>
+/deep/.v-tab {
+  font-family: Scheherazade, Arial, Helvetica, sans-serif;
+  font-size: 3rem;
+  font-weight: bold;
+  letter-spacing: 0.1rem;
+}
+</style>
