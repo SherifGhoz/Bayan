6be42d6e55aa902d231cfe253df946a6ca443448
SherifGhoz <ghozdev@gmail.com>
2020-09-01 09:53:40 +0200

    simple intro dawah

---
+++ components/Dawah/Intro.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-row>
+    <v-col
+      >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia ea,
+      dolorum impedit doloribus nulla porro sed aperiam optio eveniet suscipit
+      corrupti expedita odio aliquam. Quia corrupti explicabo doloribus vel
+      nihil.</v-col
+    >
+    <v-col
+      ><v-img src="https://via.placeholder.com/200x200.png?text=coming-soon">
+      </v-img
+    ></v-col>
+  </v-row>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
--- pages/dawah.vue
+++ pages/dawah.vue
@@ -1,7 +1,22 @@
 <template>
-  <h2>بيان الدعوية</h2>
+  <div>
+    <Intro />
+    <v-divider class="my-8 mx-auto" />
+  </div>
 </template>
 
 <script>
-export default {}
+import Intro from '~/components/Dawah/Intro'
+
+export default {
+  components: {
+    Intro
+  }
+}
 </script>
+
+<style scoped>
+::v-deep.v-divider {
+  width: 200px;
+}
+</style>
