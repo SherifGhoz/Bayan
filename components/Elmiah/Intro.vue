3804f916f96047247389c67098a131d3ad721a31
SherifGhoz <ghozdev@gmail.com>
2020-09-01 10:46:34 +0200

    Elmiah Intro

---
+++ components/Elmiah/Intro.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-row align="center" justify="space-between">
+    <v-col>
+      <span class="display-1">
+        لوريم ايبسوم دولار سيت أميت ,كونسيكتيتور أدايبا يسكينج أليايت,سيت دو
+        أيوسمود تيمبور أنكايديديونتيوت لابوري ات دولار ماجنا أليكيوا . يوت انيم
+        أد مينيم فينايم,كيواس نوستريد أكسير سيتاشن يللأمكو لابورأس نيسي يت
+        أليكيوب أكس أيا كوممودو كونسيكيوات .
+      </span></v-col
+    >
+    <v-col>
+      <Lottie :options="defaultOptions" :height="300" :width="300" />
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import Lottie from 'vue-lottie'
+import * as animationData from '~/assets/teacher-2.json'
+
+export default {
+  components: {
+    Lottie
+  },
+  data() {
+    return {
+      defaultOptions: { animationData: animationData.default }
+    }
+  }
+}
+</script>
+
+<style></style>
