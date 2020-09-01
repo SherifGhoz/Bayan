66b1c2ef5603dcbf7078acad9f67c8902769fb09
SherifGhoz <ghozdev@gmail.com>
2020-09-01 11:14:27 +0200

    intro transition

--- components/Elmiah/Intro.vue
+++ components/Elmiah/Intro.vue
@@ -1,17 +1,19 @@
 <template>
-  <v-row align="center" justify="space-between">
-    <v-col>
-      <span class="display-1">
-        لوريم ايبسوم دولار سيت أميت ,كونسيكتيتور أدايبا يسكينج أليايت,سيت دو
-        أيوسمود تيمبور أنكايديديونتيوت لابوري ات دولار ماجنا أليكيوا . يوت انيم
-        أد مينيم فينايم,كيواس نوستريد أكسير سيتاشن يللأمكو لابورأس نيسي يت
-        أليكيوب أكس أيا كوممودو كونسيكيوات .
-      </span></v-col
-    >
-    <v-col>
-      <Lottie :options="defaultOptions" :height="300" :width="300" />
-    </v-col>
-  </v-row>
+  <v-slide-y-transition mode="out-in">
+    <v-row v-show="show" align="center" justify="space-between">
+      <v-col>
+        <span class="display-1">
+          لوريم ايبسوم دولار سيت أميت ,كونسيكتيتور أدايبا يسكينج أليايت,سيت دو
+          أيوسمود تيمبور أنكايديديونتيوت لابوري ات دولار ماجنا أليكيوا . يوت
+          انيم أد مينيم فينايم,كيواس نوستريد أكسير سيتاشن يللأمكو لابورأس نيسي
+          يت أليكيوب أكس أيا كوممودو كونسيكيوات .
+        </span>
+      </v-col>
+      <v-col>
+        <Lottie :options="defaultOptions" :height="300" :width="300" />
+      </v-col>
+    </v-row>
+  </v-slide-y-transition>
 </template>
 
 <script>
@@ -24,10 +26,12 @@ export default {
   },
   data() {
     return {
+      show: false,
       defaultOptions: { animationData: animationData.default }
     }
+  },
+  mounted() {
+    this.show = true
   }
 }
 </script>
-
-<style></style>
